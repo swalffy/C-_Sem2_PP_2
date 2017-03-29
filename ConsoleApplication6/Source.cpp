4c89b6fc1625f2910944780f969dcb24d092cf36
Swalffy <vovan-tem@mail.ru>
2017-03-29 11:54:37 +0300

    Overload

---
+++ ConsoleApplication6/Source.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+class line
+{
+private:
+	char *str;
+	int len;
+public:
+	line() {
+		str = NULL;
+		len = 0;
+	}
+	line(char *x) {
+		len = strlen(x) + 1;
+		str = new char[len];
+		strcpy(str, x);
+	}
+	int size() {
+		return len;
+	}
+	~line() {
+		delete[]str;
+	}
+	friend ostream& operator<< (ostream &os, line &obj) {
+		for (int i = 0; i < obj.size(); i++)
+			os << obj.str[i];
+		return os;
+	}
+	friend istream& operator>> (istream& is, line& obj)
+	{
+		is >> obj.str;
+		obj.len = strlen(obj.str) + 1;
+		return is;
+	}
+
+	void setData(char* input)
+	{
+		str = input;
+		len = strlen(input) + 1;
+	}
+	line mergeLine(line& a, line& b)
+	{
+		a.len = len + b.len - 1;
+		char *temp = new char[len];;
+		strcpy(temp, str);
+		strcat(temp, b.str);
+		line result(temp);
+		delete[]temp;
+		return result;
+	}
+	line& operator+ (line& x)
+	{
+		
+	}
+};
+
+int main(int argc, char *argv[]) {
+	line a("Blablabla");
+	a.setData("2132165  asdas");
+	cout << a << endl;
+	line b;
+	line c;
+	b.setData("123");
+	c.mergeLine(a, b);
+	cin >> a;
+	system("pause");
+	return 0;
+}
